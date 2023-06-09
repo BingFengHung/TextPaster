4393a919142fdd9e3562842931945a23a0c062b9
BingFengHung <joe94008@gmail.com>
2023-06-09 08:48:44 +0800

    <feat>: 建立應用程式的 NotifyIcon

--- src/TextPaster/App.xaml.cs
+++ src/TextPaster/App.xaml.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Drawing;
 using System.Windows;
 
 namespace TextPaster
@@ -13,5 +9,48 @@ namespace TextPaster
     /// </summary>
     public partial class App : Application
     {
+        private readonly System.Windows.Forms.NotifyIcon notifyIcon;
+
+        public App()
+        {
+            notifyIcon = new System.Windows.Forms.NotifyIcon();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            MainWindow = new Window();
+
+            notifyIcon.Icon = new System.Drawing.Icon(@".\icon.ico");
+            notifyIcon.Text = "Text Paster";
+            notifyIcon.Visible = true;
+            notifyIcon.MouseDown += NotifyIcon_MouseDown; ;
+
+            notifyIcon.ContextMenuStrip = new System.Windows.Forms.ContextMenuStrip();
+            notifyIcon.ContextMenuStrip.Items.Add("Dashboard", Image.FromFile(@".\icon.ico"), OnStart);
+            notifyIcon.ContextMenuStrip.Items.Add("Exit", Image.FromFile(@".\icon.ico"), OnExit);
+        }
+
+        private void NotifyIcon_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                MainWindow.WindowState = WindowState.Normal;
+                MainWindow.Activate();
+                MainWindow.Show();
+            }
+        }
+
+        private void OnStart(object sender, EventArgs e)
+        {
+            MainWindow.Activate();
+            MainWindow.Show();
+        }
+
+        private void OnExit(object sender, EventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
     }
 }
